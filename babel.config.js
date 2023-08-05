690cceb1a0850ec3e0d9cc9d43bf814b8d335046
kitety <aizaizuori@foxmail.com>
2023-08-06 00:00:27 +0800

    feat: add babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/utils': './src/utils',
+          '@/pages': './src/pages',
+          '@/navigator': './src/navigator',
+          '@/models': './src/models',
+          '@/component': './src/component',
+          '@/config': './src/config',
+          '@/assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {test} from '@/utils/index';
+
+test();
+const App = () => {
+  return (
+    <View>
+      <Text>Test</Text>
+    </View>
+  );
+};
+export default App;
