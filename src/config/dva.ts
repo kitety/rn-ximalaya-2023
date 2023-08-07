b06ac43c147651ad1a6b7e4599680de5db642e2b
kitety <aizaizuori@foxmail.com>
2023-08-07 22:57:30 +0800

    feat: add dva immer

--- src/config/dva.ts
+++ src/config/dva.ts
@@ -1,12 +1,15 @@
 // 创建实例
 import models from '../models';
 import {create} from 'dva-core-ts';
+import createLoading from 'dva-loading-ts';
 
 const app = create();
 // 加载对象
 models.forEach(model => {
   app.model(model);
 });
+app.use(createLoading());
+app.use(require('dva-immer').default());
 // 启动dva
 app.start();
 // 导出对象
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,8 +1,10 @@
 import homeModel from './home';
+import {DvaLoadingState} from 'dva-loading-ts';
 
 const models = [homeModel];
 
 export type RootState = {
   home: typeof homeModel.state;
+  loading: DvaLoadingState;
 };
 export default models;
